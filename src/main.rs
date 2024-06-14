cffc8db931a2c24a8abf4dabf8c402b80a652174
EsinShadrach <esinshadrachrafe@gmail.com>
2024-06-14 23:57:01 +0100

    DOne

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c {
+                '(' | '[' | '{' => stack.push(c),
+                ')' => {
+                    if stack.pop() != Some('(') {
+                        return false;
+                    }
+                }
+                ']' => {
+                    if stack.pop() != Some('[') {
+                        return false;
+                    }
+                }
+                '}' => {
+                    if stack.pop() != Some('{') {
+                        return false;
+                    }
+                }
+                _ => return false,
+            }
+        }
+        stack.is_empty()
+    }
+}
+
 fn main() {
+    let is_valid = Solution::is_valid("()[]{}".to_string());
+    println!("{is_valid}");
     println!("Hello, world!");
 }
